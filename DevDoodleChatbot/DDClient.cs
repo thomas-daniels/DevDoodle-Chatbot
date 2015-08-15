c73727820e1215f87a47fbac0833fcc9f32fb843
ProgramFOX <programfox@hotmail.be>
2015-08-15 16:19:01 +0200

    Simplify class name
    HttpWebRequest.CreateHttp -> WebRequest.CreateHttp

--- DevDoodleChatbot/DDClient.cs
+++ DevDoodleChatbot/DDClient.cs
@@ -14,7 +14,7 @@ namespace DevDoodleChatbot
         public void Login(string name, string password)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            HttpWebRequest req = HttpWebRequest.CreateHttp("https://devdoodle.net/login/");
+            HttpWebRequest req = WebRequest.CreateHttp("https://devdoodle.net/login/");
             req.Method = "POST";
             req.ContentType = "application/x-www-form-urlencoded";
             req.Referer = "https://devdoodle.net/";
